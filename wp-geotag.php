29a9e4186f8b7e66886b12c54f22d4f693cbbc45
Daniel Y <d1098abc@gmail.com>
2014-11-08 12:20:33 +0800

    add main plugin file

---
+++ wp-geotag.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: WP Geotag
+* Plugin URI: http://www.daniel-yang.com/wp-geotag
+* Description: Add geotags to posts and pages
+* Version: 1.0
+* Author: Daniel Yang
+* Author URI: http://www.daniel-yang.com
+**/
