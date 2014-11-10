132de4bb5be11a98e2af35a5fcf493dc23aea243
Daniel Y <d1098abc@gmail.com>
2014-11-10 20:26:40 +0800

    save latitude and longitude values for posts

--- wp-geotag.php
+++ wp-geotag.php
@@ -14,7 +14,7 @@ add_action( 'load-post-new.php', 'geotag_meta_boxes_setup' );
 
 function geotag_meta_boxes_setup() {
 	add_action( 'add_meta_boxes', 'geotag_add_post_meta_boxes' );
-	add_action( 'save_post', 'save_geotag_info_meta');
+	add_action( 'save_post', 'save_geotag_info_meta', 10, 2);
 }
 
 function geotag_add_post_meta_boxes() {
@@ -42,3 +42,35 @@ function geotag_post_class_meta_box( $object, $box ) { ?>
 	<input class="widefat" type="text" name="geotag-longitude" id="geotag-longitude" value="<?php echo esc_attr( get_post_meta( $object->ID, 'geotag_longitude', true ) ); ?>" size="30" />
 </p>
 <?php }
+
+function save_geotag_info_meta( $post_id, $post ) {
+
+  if ( !isset( $_POST['geotag_post_class_nonce'] ) || !wp_verify_nonce( $_POST['geotag_post_class_nonce'], basename( __FILE__ ) ) )
+    return $post_id;
+
+  $post_type = get_post_type_object( $post->post_type );
+
+  $fields = array('geotag-latitude', 'geotag-longitude');
+  
+  foreach($fields as $field)
+  {
+	  if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
+	    return $post_id;
+
+	  $new_meta_value = ( isset( $_POST[$field] ) ? sanitize_html_class( $_POST[$field] ) : '' );
+
+	  $meta_key = str_replace('-','_',$field);
+
+	  $meta_value = get_post_meta( $post_id, $meta_key, true );
+
+	  if ( $new_meta_value && '' == $meta_value )
+	    add_post_meta( $post_id, $meta_key, $new_meta_value, true );
+
+	  elseif ( $new_meta_value && $new_meta_value != $meta_value )
+	    update_post_meta( $post_id, $meta_key, $new_meta_value );
+
+	  elseif ( '' == $new_meta_value && $meta_value )
+	    delete_post_meta( $post_id, $meta_key, $meta_value );
+  }
+  
+}
