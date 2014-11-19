accc1852ce0a7a0d308a5828ceb3c2d3753d5059
Daniel Y <d1098abc@gmail.com>
2014-11-19 22:15:23 +0800

    code formatting

--- wp-geotag.php
+++ wp-geotag.php
@@ -23,128 +23,128 @@ function wp_geotag_map() {
 		<script type=\"text/javascript\">
 	function initialize() {
 		var myLatlng = new google.maps.LatLng(".get_post_meta( get_the_ID(), 'geotag_latitude', true ) .",".esc_attr( get_post_meta( get_the_ID(), 'geotag_longitude', true ) ).");
-	        var mapOptions = {
-	          center: myLatlng,
-	          zoom: 8
-	        };
-	        var map = new google.maps.Map(document.getElementById('wp-geotag-map'),
-	            mapOptions);
+		var mapOptions = {
+			center: myLatlng,
+			zoom: 8
+		};
+		var map = new google.maps.Map(document.getElementById('wp-geotag-map'),
+		mapOptions);
 				
-	  		  var marker = new google.maps.Marker({
-	  		      position: myLatlng,
-	  		      map: map,
-	  		      title:\"".get_the_title()."\"
-	  		  });
-	      }
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			title:\"".get_the_title()."\"
+			});
+		}
 		  
-	      google.maps.event.addDomListener(window, 'load', initialize);
+		google.maps.event.addDomListener(window, 'load', initialize);
 		  
 		  
 		  
-	</script>";
-}
-add_shortcode( 'wp_geotag_map', 'wp_geotag_map' );
+		</script>";
+	}
+	add_shortcode( 'wp_geotag_map', 'wp_geotag_map' );
 
-function geotag_meta_boxes_setup() {
-	add_action( 'add_meta_boxes', 'geotag_add_post_meta_boxes' );
-	add_action( 'save_post', 'save_geotag_info_meta', 10, 2);
-}
+	function geotag_meta_boxes_setup() {
+		add_action( 'add_meta_boxes', 'geotag_add_post_meta_boxes' );
+		add_action( 'save_post', 'save_geotag_info_meta', 10, 2);
+	}
 
-function geotag_add_post_meta_boxes() {
-	$post_types = array('post','page');
-	foreach ($post_types as $post_type)
-	{
-		add_meta_box(
-		'wp_geotag',      // Unique ID
-		esc_html__( 'Geotag', 'example' ),    // Title
-		'geotag_post_class_meta_box',   // Callback function
-		$post_type,         // Admin page (or post type)
-		'side',         // Context
-		'default'         // Priority
+	function geotag_add_post_meta_boxes() {
+		$post_types = array('post','page');
+		foreach ($post_types as $post_type)
+		{
+			add_meta_box(
+			'wp_geotag',      // Unique ID
+			esc_html__( 'Geotag', 'example' ),    // Title
+			'geotag_post_class_meta_box',   // Callback function
+			$post_type,         // Admin page (or post type)
+			'side',         // Context
+			'default'         // Priority
 		);
 	}
 }
 
 function geotag_post_class_meta_box( $object, $box ) { ?>
 
-<?php wp_nonce_field( basename( __FILE__ ), 'geotag_post_class_nonce' ); ?>
+	<?php wp_nonce_field( basename( __FILE__ ), 'geotag_post_class_nonce' ); ?>
 
-<p>
-	<label for="geotag-latitude"><?php _e( "Latitude", 'example' ); ?></label>
-	<br />
-	<input class="widefat" type="text" name="geotag-latitude" id="geotag-latitude" value="<?php echo esc_attr( get_post_meta( $object->ID, 'geotag_latitude', true ) ); ?>" size="30" />
-	<br />
-	<label for="geotag-longitude"><?php _e( "Longitude", 'example' ); ?></label>
-	<br />
-	<input class="widefat" type="text" name="geotag-longitude" id="geotag-longitude" value="<?php echo esc_attr( get_post_meta( $object->ID, 'geotag_longitude', true ) ); ?>" size="30" />
-</p>
-<?php }
+	<p>
+		<label for="geotag-latitude"><?php _e( "Latitude", 'example' ); ?></label>
+		<br />
+		<input class="widefat" type="text" name="geotag-latitude" id="geotag-latitude" value="<?php echo esc_attr( get_post_meta( $object->ID, 'geotag_latitude', true ) ); ?>" size="30" />
+		<br />
+		<label for="geotag-longitude"><?php _e( "Longitude", 'example' ); ?></label>
+		<br />
+		<input class="widefat" type="text" name="geotag-longitude" id="geotag-longitude" value="<?php echo esc_attr( get_post_meta( $object->ID, 'geotag_longitude', true ) ); ?>" size="30" />
+	</p>
+	<?php }
 
-function save_geotag_info_meta( $post_id, $post ) {
+	function save_geotag_info_meta( $post_id, $post ) {
 
-  if ( !isset( $_POST['geotag_post_class_nonce'] ) || !wp_verify_nonce( $_POST['geotag_post_class_nonce'], basename( __FILE__ ) ) )
-    return $post_id;
+		if ( !isset( $_POST['geotag_post_class_nonce'] ) || !wp_verify_nonce( $_POST['geotag_post_class_nonce'], basename( __FILE__ ) ) )
+			return $post_id;
 
-  $post_type = get_post_type_object( $post->post_type );
+		$post_type = get_post_type_object( $post->post_type );
 
-  $fields = array('geotag-latitude', 'geotag-longitude');
+		$fields = array('geotag-latitude', 'geotag-longitude');
   
-  foreach($fields as $field)
-  {
-	  if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
-	    return $post_id;
+		foreach($fields as $field)
+		{
+			if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
+				return $post_id;
 
-	  $new_meta_value = ( isset( $_POST[$field] ) ?  $_POST[$field] : '' );
+			$new_meta_value = ( isset( $_POST[$field] ) ?  $_POST[$field] : '' );
 
-	  $meta_key = str_replace('-','_',$field);
+			$meta_key = str_replace('-','_',$field);
 
-	  $meta_value = get_post_meta( $post_id, $meta_key, true );
+			$meta_value = get_post_meta( $post_id, $meta_key, true );
 
-	  if ( $new_meta_value && '' == $meta_value )
-	    add_post_meta( $post_id, $meta_key, $new_meta_value, true );
+			if ( $new_meta_value && '' == $meta_value )
+				add_post_meta( $post_id, $meta_key, $new_meta_value, true );
 
-	  elseif ( $new_meta_value && $new_meta_value != $meta_value )
-	    update_post_meta( $post_id, $meta_key, $new_meta_value );
+			elseif ( $new_meta_value && $new_meta_value != $meta_value )
+				update_post_meta( $post_id, $meta_key, $new_meta_value );
 
-	  elseif ( '' == $new_meta_value && $meta_value )
-	    delete_post_meta( $post_id, $meta_key, $meta_value );
-  }
+			elseif ( '' == $new_meta_value && $meta_value )
+				delete_post_meta( $post_id, $meta_key, $meta_value );
+		}
   
-}
+	}
 
 
-// create custom plugin settings menu
-add_action('admin_menu', 'wp_geotag_create_menu');
+	// create custom plugin settings menu
+	add_action('admin_menu', 'wp_geotag_create_menu');
 
-function wp_geotag_create_menu() {
+	function wp_geotag_create_menu() {
 
-	add_options_page('WP Geotag Plugin Settings', 'WP Geotag', 'administrator', __FILE__, 'wp_geotag_settings_page');
+		add_options_page('WP Geotag Plugin Settings', 'WP Geotag', 'administrator', __FILE__, 'wp_geotag_settings_page');
 
-	add_action( 'admin_init', 'register_mysettings' );
-}
+		add_action( 'admin_init', 'register_mysettings' );
+	}
 
 
-function register_mysettings() {
-	register_setting( 'wp-geotag-settings-group', 'wp_geotag_gmaps_api_key' );
-}
+	function register_mysettings() {
+		register_setting( 'wp-geotag-settings-group', 'wp_geotag_gmaps_api_key' );
+	}
 
-function wp_geotag_settings_page() {
-?>
-<div class="wrap">
-<h2>WP Geotag Settings</h2>
+	function wp_geotag_settings_page() {
+		?>
+		<div class="wrap">
+			<h2>WP Geotag Settings</h2>
 
-<form method="post" action="options.php">
- <?php settings_fields( 'wp-geotag-settings-group' ); ?>
-    <?php do_settings_sections( 'wp-geotag-settings-group' ); ?>
-    <table class="form-table">
-        <tr valign="top">
-        <th scope="row">Google Maps API Key</th>
-        <td><input type="text" name="wp_geotag_gmaps_api_key" value="<?php echo esc_attr( get_option('wp_geotag_gmaps_api_key') ); ?>" /></td>
-        </tr>
-    </table>
+			<form method="post" action="options.php">
+				<?php settings_fields( 'wp-geotag-settings-group' ); ?>
+				<?php do_settings_sections( 'wp-geotag-settings-group' ); ?>
+				<table class="form-table">
+					<tr valign="top">
+						<th scope="row">Google Maps API Key</th>
+						<td><input type="text" name="wp_geotag_gmaps_api_key" value="<?php echo esc_attr( get_option('wp_geotag_gmaps_api_key') ); ?>" /></td>
+					</tr>
+				</table>
     
-    <?php submit_button(); ?>
+				<?php submit_button(); ?>
 
-</form>
-</div>
-<?php } ?>
+			</form>
+		</div>
+		<?php } ?>
